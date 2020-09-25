8142c205199d230bdde33442bc9c42f0b8d7b9d6
Cornelia Johnson <corneliaj91@gmail.com>
2020-09-25 14:10:46 -0400

    added time format

--- src/scripts/messages/MessageHTML.js
+++ src/scripts/messages/MessageHTML.js
@@ -7,7 +7,11 @@ export const MessageHTML = (messageObj) => {
   <a href="#"><div class="userIcon shadow">${messageObj.user.username}</div></a>
     <div class="messageText">${messageObj.message}</div>
     <div class="messageDetail">${deleteBtn(messageObj)}
-    <div>${messageObj.date}</div>
+    <div>${new Date(messageObj.date * 1000).toLocaleString("en-US", {
+      weekday: "short",
+      hour: "numeric",
+      minute: "numeric",
+    })}</div>
     </div>
   </div>
   </section>
