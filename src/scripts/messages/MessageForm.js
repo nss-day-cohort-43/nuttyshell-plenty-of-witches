15d12fbdc8263abcc79e3408118923ad961202e9
Cornelia Johnson <corneliaj91@gmail.com>
2020-09-24 20:39:33 -0400

    added modal

--- src/scripts/messages/MessageForm.js
+++ src/scripts/messages/MessageForm.js
@@ -1,14 +1,35 @@
+const eventHub = document.querySelector(".container");
 const contentTarget = document.querySelector(".messageContainer");
+
 export const MessageForm = () => {
   return (contentTarget.innerHTML = `
   <div class="message--history"></div>
   <div class="message--entry">
-    <div class="message--input">
-      <input type="text" class="message--inputbox" />
-    </div>
+    <div class="message--input"></div>
     <div>
-      <button class="postMessageBtn">Send</button>
+      <button id="postMessageBtn">Send</button>
     </div>
   </div>
+  <div class="modal">
+  <div class="modal-content">
+    <span class="close-btn closeMessage">&times;</span>
+    <h3>Add A Post</h3>
+    <input id="message-textarea"></input>
+    <div>
+    <button class="closeMessage">Cancel</button>
+    <button>Post</button>
+    </div>
+  </div>
+</div>
   `);
 };
+
+eventHub.addEventListener("click", (event) => {
+  const modal = document.querySelector(".modal");
+  if (event.target.id === "postMessageBtn") {
+    modal.style.display = "block";
+  }
+  if (event.target.classList.contains("closeMessage")) {
+    modal.style.display = "none";
+  }
+});
