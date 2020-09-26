172bacd907a8182fdcdb9effb497be0f6fcbcd4b
Parker Kelley <Pklly92@gmail.com>
2020-09-25 22:03:38 -0500

    got the save button fixed.

--- src/scripts/events/EventDataProvider.js
+++ src/scripts/events/EventDataProvider.js
@@ -3,17 +3,25 @@ let localEvents = [];
 
 // This will be for grabbing the events from our local json server -
 export const getEvents = () => {
-	return fetch('http://localhost:8088/events')
-		.then((Response) => Response.json())
-		.then((parsedEvents) => {
-			localEvents = parsedEvents;
-		});
+  return fetch('http://localhost:8088/events')
+    .then((Response) => Response.json())
+    .then((parsedEvents) => {
+      localEvents = parsedEvents;
+    });
 };
 
 // This will return a copy of the localEvents so that we can change it without changing the original array.
 export const useEvents = () => {
-	localEvents.slice();
+  localEvents.slice();
 };
 
 // We will need a saveEvents() - which will push the new user created event.
-// export const saveEvent = (Event) => {};
+export const saveEvents = (event) => {
+  return fetch('http://localhost:8088/events', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(event),
+  });
+  // .then we want to update our aside bar
+  // .then dispatch an event that says we need to re-render the aside
+};
--- src/scripts/events/EventForm.js
+++ src/scripts/events/EventForm.js
@@ -1,16 +1,92 @@
+import { getEvents, saveEvents } from './EventDataProvider.js';
+
+const stateArray = [
+  'Alabama',
+  'Alaska',
+  'American Samoa',
+  'Arizona',
+  'Arkansas',
+  'California',
+  'Colorado',
+  'Connecticut',
+  'Delaware',
+  'District of Columbia',
+  'Federated States of Micronesia',
+  'Florida',
+  'Georgia',
+  'Guam',
+  'Hawaii',
+  'Idaho',
+  'Illinois',
+  'Indiana',
+  'Iowa',
+  'Kansas',
+  'Kentucky',
+  'Louisiana',
+  'Maine',
+  'Marshall Islands',
+  'Maryland',
+  'Massachusetts',
+  'Michigan',
+  'Minnesota',
+  'Mississippi',
+  'Missouri',
+  'Montana',
+  'Nebraska',
+  'Nevada',
+  'New Hampshire',
+  'New Jersey',
+  'New Mexico',
+  'New York',
+  'North Carolina',
+  'North Dakota',
+  'Northern Mariana Islands',
+  'Ohio',
+  'Oklahoma',
+  'Oregon',
+  'Palau',
+  'Pennsylvania',
+  'Puerto Rico',
+  'Rhode Island',
+  'South Carolina',
+  'South Dakota',
+  'Tennessee',
+  'Texas',
+  'Utah',
+  'Vermont',
+  'Virgin Island',
+  'Virginia',
+  'Washington',
+  'West Virginia',
+  'Wisconsin',
+  'Wyoming',
+];
+
+const renderStateDropDown = (stateArray) => {
+  return `${stateArray
+    .map((state) => {
+      return `<option value="${state}">${state}</option>`;
+    })
+    .join(' ')}`;
+};
+
 const eventHub = document.querySelector('.dashboard');
 
 export const renderNewEventForm = () => {
-	// this will render the new event form - so it needs to render a new form with fields for the user to enter
-	let eventsContainerTarget = document.querySelector('.eventsContainer');
-	eventsContainerTarget.innerHTML = `
+  // this will render the new event form - so it needs to render a new form with fields for the user to enter
+  let eventsContainerTarget = document.querySelector('.eventsContainer');
+  eventsContainerTarget.innerHTML = `
   <section class="newUserEventForm">
             <h4>Please enter the name for your event</h4>
             <input id="newUserEvent--eventName" type="text" placeholder="Enter your event name">
             <h4>Please enter the city that your event is in</h4>
             <input id="newUserEvent--eventLocationCity" type="text" placeholder="Enter the events city">
-            <h4>Please enter the state that your event is in</h4>
-            <input id="newUserEvent--eventLocationState" type="text" placeholder="Enter the events state">
+						<h4>Please enter the state that your event is in</h4>
+						<label for="stateDropDown">State</label>
+						<select name="stateDropDown" id="stateDropDown">
+							<option value="0">Pick a State</option>
+  						${renderStateDropDown(stateArray)}
+						</select>
             <h4>Please enter the Zip that your event is in</h4>
             <input id="newUserEvent--eventLocationZip" type="text" placeholder="Enter the events zip code">
             <h4>Please enter the event's date</h4>
@@ -21,20 +97,41 @@ export const renderNewEventForm = () => {
 };
 
 eventHub.addEventListener('click', (clickEvent) => {
-	if (clickEvent.target.id.includes('--createEventButton')) {
-		const savedUserEventName = document.querySelector(
-			'#newUserEvent--eventName'
-		).value;
-		const savedUserEventCity = document.querySelector(
-			'#newUserEvent--eventLocationCity'
-		).value;
-		const savedUserEventState = document.querySelector(
-			'#newUserEvent--eventLocationState'
-		).value;
-		const savedUserEventZip = document.querySelector(
-			'#newUserEvent--eventLocationZip'
-		).value;
-		const savedUserEventDate = document.querySelector('#newUserEvent--date')
-			.value;
-	}
+  if (clickEvent.target.id.includes('--createEventButton')) {
+    const savedUserEventName = document.querySelector(
+      '#newUserEvent--eventName'
+    ).value;
+    const savedUserEventCity = document.querySelector(
+      '#newUserEvent--eventLocationCity'
+    ).value;
+    const savedUserEventState = document.querySelector('#stateDropDown').value;
+    const savedUserEventZip = document.querySelector(
+      '#newUserEvent--eventLocationZip'
+    ).value;
+    const savedUserEventDate = document.querySelector('#newUserEvent--date')
+      .value;
+    const savedLoggedInUserId = sessionStorage.getItem('activeUser');
+
+    if (
+      savedUserEventName === '' ||
+      savedUserEventCity === '' ||
+      savedUserEventState === '0' ||
+      savedUserEventZip === '' ||
+      savedUserEventZip.length > 5 ||
+      // savedUserEventZip.test(/^[0-9]+$/) === false ||
+      savedUserEventDate === ''
+    ) {
+      window.alert('Yo fill out yo shit witch');
+    } else {
+      let newUserEvent = {
+        userId: savedLoggedInUserId,
+        eventName: savedUserEventName,
+        eventLocationCity: savedUserEventCity,
+        eventLocationState: savedUserEventState,
+        eventLocationZip: savedUserEventZip,
+        date: savedUserEventDate,
+      };
+      saveEvents(newUserEvent).then(getEvents).then(renderNewEventForm);
+    }
+  }
 });
